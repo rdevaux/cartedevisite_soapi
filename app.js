593d41a1d2e5bab4e91acd25ab247a5b9f9765fb
Romain Devaux <r.devaux@icloud.com>
2022-01-28 13:55:04 +0100

    ajout du footer + optimisation du sass + maj du javascript

--- app.js
+++ app.js
@@ -1,5 +1,19 @@
+// Gestion de la navbar
+
 const btn = document.querySelector('.btn1');
 const menu = document.querySelector('.liste-nav');
 
 btn.addEventListener('click', () => btn.classList.toggle('active'));
 btn.addEventListener('click', () => menu.classList.toggle('active-menu'));
+
+// Affichage/Masquage du btn-up
+
+const btnUP = document.querySelector('#btn-up');
+
+window.addEventListener("scroll", (event) => {
+    let scroll = this.scrollY;
+    // console.log(scroll);
+
+    if(scroll < 650) btnUP.style.display = "none";
+    else btnUP.style.display = "flex";
+});
